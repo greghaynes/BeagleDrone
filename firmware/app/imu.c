45b4495682c0c87e8cb6fb871ea195e98b1d9180
Gregory Haynes <greg@greghaynes.net>
2013-02-24 02:30:39 -0800

    Switch to I2C1 for IMU

--- firmware/app/imu.c
+++ firmware/app/imu.c
@@ -6,24 +6,24 @@
 #define I2C_SLAVE_ADDR 0x00
 
 void IMUInit(void) {
-    // Enable the clock for I2C0
-    I2C0ModuleClkConfig();
+    // Enable the clock for I2C1
+    I2C1ModuleClkConfig();
 
-    // Set pin mux for I2C0
-    I2CPinMuxSetup(0);
+    // Set pin mux for I2C1
+    I2CPinMuxSetup(1);
 
-    // Put I2C0 in reset/disabled state
-    I2CMasterDisable(SOC_I2C_0_REGS);
+    // Put I2C1 in reset/disabled state
+    I2CMasterDisable(SOC_I2C_1_REGS);
 
     // Disable auto idle
-    I2CAutoIdleDisable(SOC_I2C_0_REGS);
+    I2CAutoIdleDisable(SOC_I2C_1_REGS);
 
     // Set bus speed
-    I2CMasterInitExpClk(SOC_I2C_0_REGS, 48000000, 12000000, 100000);
+    I2CMasterInitExpClk(SOC_I2C_1_REGS, 48000000, 12000000, 100000);
 
     // Set slave address
-    I2CMasterSlaveAddrSet(SOC_I2C_0_REGS, I2C_SLAVE_ADDR);
+    I2CMasterSlaveAddrSet(SOC_I2C_1_REGS, I2C_SLAVE_ADDR);
 
     // Bring out of reset
-    I2CMasterEnable(SOC_I2C_0_REGS);
+    I2CMasterEnable(SOC_I2C_1_REGS);
 }
