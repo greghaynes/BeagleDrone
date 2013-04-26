27af8c7bffc001d209b56b97c26759fb02b7aa75
Gregory Haynes <greg@greghaynes.net>
2013-04-25 23:35:12 -0700

    Added asserts to test_basic_quaternion

--- firmware/app/mathtypes.h
+++ firmware/app/mathtypes.h
@@ -12,5 +12,8 @@ typedef struct Vector4F {
 void Vector3FZero(Vector3F *v);
 void Vector4FZero(Vector4F *v);
 
+int Vector3FEqual(const Vector3F *a, const Vector3F *b);
+int Vector4FEqual(const Vector4F *a, const Vector4F *b);
+
 #endif
 
--- firmware/tests/test_quaternion.c
+++ firmware/tests/test_quaternion.c
@@ -2,6 +2,7 @@
 #include "app/quaternion.h"
 
 #include <stdio.h>
+#include <assert.h>
 
 void QuaternionPrint(const Quaternion *q) {
     printf("Quaternion: %f\t%f\t%f\t%f\n",
@@ -10,9 +11,20 @@ void QuaternionPrint(const Quaternion *q) {
 
 void test_basic_quaternion_from_eulers(void) {
     Quaternion q = { 0, 0, 0, 0 };
+
     Vector3F eulers = { 0, 0, 0 };
     QuaternionFromEulers(&eulers, &q);
     QuaternionPrint(&q);
+    Vector4F res = { 1, 0, 0, 0 };
+    assert(Vector4FEqual(&res, &q));
+
+    eulers.a = 3.14159265;
+    QuaternionFromEulers(&eulers, &q);
+    QuaternionPrint(&q);
+    assert(q.a < .0001 && q.a > -.0001);
+    assert(q.b < 1.0001 && q.b > .9999);
+    assert(q.c < .0001 && q.c > -.0001);
+    assert(q.c < .0001 && q.c > -.0001);
 }
 
 int main(int argc, char **argv) {
