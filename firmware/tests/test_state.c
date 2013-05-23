bc139db3093f0ab3c5df3df3b0667dfd1fd15fdd
Gregory Haynes <greg@greghaynes.net>
2013-05-23 14:44:27 -0500

    Added large angvel test

--- firmware/tests/test_state.c
+++ firmware/tests/test_state.c
@@ -6,6 +6,7 @@
 
 void StatePrint(const State *state) {
     const Quaternion *q = &state->r_b_to_i;
+    printf("state:\t");
     printf("a: %f\tb: %f\tc: %f\td: %f\n",
            q->a, q->b, q->c, q->d);
     Vector3F eulers;
@@ -14,7 +15,7 @@ void StatePrint(const State *state) {
             eulers.a, eulers.b, eulers.c);
 }
 
-void test_basic_state_angvel_update(void) {
+void test_single_axis_basic_angvel_update(void) {
     State s;
     StateInit(&s);
     StatePrint(&s);
@@ -33,9 +34,26 @@ void test_basic_state_angvel_update(void) {
     assert(eulers.c == 0);
 }
 
+void test_single_axis_large_angvel_update(void) {
+    State s;
+    StateInit(&s);
+    StatePrint(&s);
+
+    int i;
+    StateRotationalFloat ang_vel = { 1, 0, 0 };
+    for(i = 0;i < 314159;i++) {
+        StateUpdateFromAngVel(&s, &ang_vel, .001);
+    }
+
+    StatePrint(&s);
+    Vector3F eulers;
+    QuaternionToEulers(&s.r_b_to_i, &eulers);
+}
+
 int main(int argc, char **argv) {
     TestInfo tests[] = {
-        { "Basic state updating", test_basic_state_angvel_update },
+        { "Basic state updating", test_single_axis_basic_angvel_update },
+        { "Single axis large rotation", test_single_axis_large_angvel_update },
         { 0, 0 }
     };
 
