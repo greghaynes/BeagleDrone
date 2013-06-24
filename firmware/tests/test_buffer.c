97283809d9ebfb8b1a114abd19e7b2dacaf1ecc6
Gregory Haynes <greg@greghaynes.net>
2013-06-23 20:38:04 -0700

    Added test for ringbuffer push

--- firmware/tests/test_buffer.c
+++ firmware/tests/test_buffer.c
@@ -33,10 +33,30 @@ void test_buffer_set(void) {
     assert(!strncmp(b.data, "bbbb", 4));
 }
 
+void test_ring_buffer_push(void) {
+    RingBuffer b;
+    char data[5];
+    int i;
+    RingBufferInit(&b, data, 5);
+    RingBufferPush(&b, 'a');
+    assert(!strncmp(b.data, "a", 1));
+
+    for(i = 0;i < 4;++i)
+        RingBufferPush(&b, 'b');
+    assert(!strncmp(b.data, "abbbb", 5));
+
+    RingBufferPush(&b, 'c');
+    assert(!strncmp(b.data, "cbbbb", 5));
+
+    RingBufferPush(&b, 'c');
+    assert(!strncmp(b.data, "ccbbb", 5));
+}
+
 int main(int argc, char **argv) {
     TestInfo tests[] = {
         { "Buffer append", test_buffer_append },
         { "Buffer set", test_buffer_set },
+        { "Ring buffer push", test_ring_buffer_push },
     };
 
     run_tests(tests);
