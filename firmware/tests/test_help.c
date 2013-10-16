3f7044a8cd20b4e6fc46e8af0ffdcfc1bb0439e2
Gregory Haynes <greg@greghaynes.net>
2013-10-16 12:31:00 -0700

    Updated test output to add per test and test set deliniation

--- firmware/tests/test_help.c
+++ firmware/tests/test_help.c
@@ -4,11 +4,17 @@
 
 void run_tests(const TestInfo tests[]) {
     const TestInfo *test = &tests[0];
+    printf("--------------------\n");
     while(test->description && test->test_function) {
         printf("Running test: %s\n", test->description);
         test->test_function();
         printf("Done\n");
+        printf("\n");
         ++test;
     }
+    printf("------------------\n");
+    printf("Test set completed\n");
+    printf("\n\n");
+
 }
 
