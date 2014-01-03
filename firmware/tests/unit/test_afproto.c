7a482917efb5337f96dca8d6bb7be4f8f45041ab
Gregory Haynes <greg@greghaynes.net>
2014-01-02 22:10:03 -0800

    Added test for afproto frame which requires CRC escaping

--- firmware/tests/unit/test_afproto.c
+++ firmware/tests/unit/test_afproto.c
@@ -88,9 +88,28 @@ void test_push_frame(void) {
     assert(framed_crc == crc16_buff(test_str, strlen(test_str)));
 }
 
+void test_push_escape_crc_frame(void) {
+    char in_data[256];
+    char out_data[512];
+    const char *test_str = "hi";
+    RingBuffer rb_in, rb_out;
+
+    RingBufferInit(&rb_in, in_data, 256);
+    RingBufferInit(&rb_out, out_data, 512);
+
+    RingBufferPushString(&rb_in, test_str);
+    afproto_ringbuffer_push_frame(&rb_out, &rb_in);
+
+    int framed_len = strlen(test_str) + escaped_bytes_in(test_str) + 5;
+
+    assert(RingBufferSize(&rb_out) == framed_len);
+    assert(!strncmp(out_data, "\x7d\x68\x69\x0c\x7e\x5f\x7f", framed_len));
+}
+
 int main(int argc, char **argv) {
     TestInfo tests[] = {
         { "Push frame", test_push_frame },
+        { "Push escape crc frame", test_push_escape_crc_frame },
         { "Pop frame", test_pop_frame },
         { "Pop no end byte", test_pop_no_end_byte },
         { "Pop invalid crc", test_pop_invalid_crc },
