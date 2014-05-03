a5e3e14a4321e5b808f021629619344d772aaee3
Gregory Haynes <greg@greghaynes.net>
2014-05-02 22:57:40 -0700

    Move kernel-specific communication code into glue

--- firmware/app/communication.c
+++ firmware/app/communication.c
@@ -1,17 +1,11 @@
-#include "kernel/hw/soc_AM335x.h"
-#include "kernel/drivers/uart_irda_cir.h"
-#include "kernel/drivers/uart.h"
-#include "kernel/interrupt.h"
 #include "app/afproto.h"
 #include "app/crc16.h"
 #include "app/log.h"
 #include "app/command.h"
 #include "app/communication.h"
+#include "glue/communication.h"
 
 #include <string.h>
-
-#define UART_CONSOLE_BASE                    (SOC_UART_0_REGS)
-#define BAUD_RATE_115200                     (115200)
 
 static CommunicationState *_communicationState;
 
@@ -31,46 +25,19 @@ void CommunicationHandleFrame(CommunicationState *com) {
 }
 
 void CommunicationCheckWrite(CommunicationState *com) {
-#ifdef DEBUG_UART
-    UARTprintf("Write called, start: %u, end: %u\n",
-               uart_out_buff_start,
-               uart_out_buff_end);
-#endif
-
     char ch;
     while(RingBufferPeek(&com->uart_out_ringbuffer, &ch)) {
-#ifdef DEBUG_UART
-        UARTPuts("Write loop, Out buff: ", 22);
-        int i;
-        for(i = uart_out_buff_start;i < uart_out_buff_end;++i)
-            UARTprintf("%x", com->uart_out_buff[i]);
-        UARTPuts("\n", 1);
-#endif
-
-        // Try to put byte in transmit buffer
-        if(UARTCharPutNonBlocking(UART_CONSOLE_BASE, ch)) {
+        if(CommunicationTrySendChar(ch))
             RingBufferPop(&com->uart_out_ringbuffer, 0);
-#ifdef DEBUG_UART
-            UARTprintf("Char written, start at %u\n",
-                       com->uart_out_buff_start);
-#endif
-        } else {
-#ifdef DEBUG_UART
-            UARTPuts("THR full, returning write\n\n", 26);
-            return;
-#endif
-        }
     }
 
-#ifdef DEBUG_UART
-    UARTPuts("Write completed\n\n", 17);
-#endif
-    UARTIntDisable(UART_CONSOLE_BASE, UART_INT_THR);
+    if(RingBufferSize(&com->uart_out_ringbuffer) > 0)
+        CommunicationIntDisableWrite();
 }
 
 void CommunicationCheckRead(CommunicationState *com) {
     unsigned char in_char;
-    while(UartGetCharNonBlocking(UART_CONSOLE_BASE, &in_char)) {
+    while(CommunicationTryGetChar(&in_char)) {
         CommunicationGotChar(com, in_char);
     }
 }
@@ -86,14 +53,8 @@ void CommunicationCheck(CommunicationState *com) {
  * a rewrite of the UART driver to be OO. For now, were storing the state
  * globally.
  */
-// Extra wrapper for ISR so we can mock out the interrupt call in testing.
-static void CommunicationISR(void *data) {
-    CommunicationState *com = (CommunicationState*)data;
-    CommunicationCheck(com);
-}
-
-static void UARTIsr(void) {
-    CommunicationISR(_communicationState);
+static void CommunicationISR(void) {
+    CommunicationCheck(_communicationState);
 }
 
 void CommunicationSend(CommunicationState *com, const char *data,
@@ -106,7 +67,7 @@ void CommunicationSend(CommunicationState *com, const char *data,
 
     afproto_ringbuffer_push_frame(&com->uart_out_ringbuffer, data, data_size);
 
-    UARTIntEnable(UART_CONSOLE_BASE, UART_INT_THR);
+    CommunicationIntEnableWrite();
 }
 
 void CommunicationGotChar(CommunicationState *com, char ch) {
@@ -124,17 +85,18 @@ void CommunicationInit(CommunicationState *com) {
             COMMUNICATION_UART_IN_BUFF_SIZE);
     RingBufferInit(&com->uart_out_ringbuffer, com->uart_out_data,
             COMMUNICATION_UART_OUT_BUFF_SIZE);
+
     _communicationState = com;
+    CommunicationIntRegisterHandler(CommunicationISR);
 
     LogCString(LOG_LEVEL_DEBUG, "Initializing communication");
 
     // Initialize UART peripheral
-    UartInit(UART_CONSOLE_BASE, BAUD_RATE_115200);
-
-    UartSetInterruptHandler(UART_CONSOLE_BASE, UARTIsr);
+    CommunicationHwInit();
 
     // Enable UART read interrupts
-    UARTIntEnable(UART_CONSOLE_BASE, UART_INT_RHR_CTI);
+    CommunicationIntEnableRead();
+
     LogCString(LOG_LEVEL_DEBUG, "Communication initialized");
 }
 
